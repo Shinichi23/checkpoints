09d63b4ef7dbd2ee5eb6c36c557d8a87fd16269b
Taha <taha-chaoui@hotmail.com>
2020-09-06 11:13:16 +0100

    new change

--- distinct.js
+++ distinct.js
@@ -1,6 +1,13 @@
-let arr1 = [];
-let arr2 = [];
+let arr1 = [2, 7, 5];
+let arr2 = [3, 7, 5];
+let arrT = [];
+let newArr = arrT.concat(arr1, arr2);
 
-function distinctEl(){
-    
+function duplicate(arr) {
+  const duplicates = arr.filter(function(elm){
+    return arr.indexOf(elm) !== arr.lastIndexOf(elm)
+  });
+  return duplicates.reduce((a, b) => a + b);
 }
+
+console.log(duplicate(newArr))
--- overlapping.js
+++ overlapping.js
@@ -4,8 +4,10 @@ let arrT = [];
 let newArr = arrT.concat(arr1, arr2);
 
 function duplicate(arr) {
-  const duplicates = arr.filter(elm => arr.indexOf(elm) !== arr.lastIndexOf(elm));
+  const duplicates = arr.filter(function(elm){
+    return arr.indexOf(elm) !== arr.lastIndexOf(elm)
+  });
   return duplicates.reduce((a, b) => a + b);
 }
 
-console.log(duplicate(newArr)) 
+console.log(duplicate(newArr))
