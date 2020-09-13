66007337058e11646a54f15613e55d3258201fd1
Taha Chaoui <taha-chaoui@hotmail.com>
2020-09-13 13:12:12 +0100

    finished

--- Checkpoint Procedural programming/procedural.js
+++ Checkpoint Procedural programming/procedural.js
@@ -1,26 +1,7 @@
+let result = 0;
 
-let vectors = [[7, 1, 1],[6, 0, 2], [5, 7, 0], [3, 1, 2], [2, 3 ,8], [0, 0, 0]]
-
-function dotProduct() {
-    for(let i = 0; i < vectors.length; i++){
-
-        for(let j = i + 1; j < vectors.length; j++){
-
-            let result = 0;
-
-                for(let k = 0; k < vectors[i].length; k++){ 
-
-                    result += vectors[i][k] * vectors[j][k]
-
-                    }
-
-    if(result === 0){
-
-        console.log(`${vectors[i]} and ${vectors[j]} are orthogonal `)
-        
+function dotProduct(vector1, vector2){
+    for(let i = 0; i < vector1.length; i++){
+        result += vector1[i] * vector2[i]
     }
-  }
 }
-}
-
-dotProduct();
