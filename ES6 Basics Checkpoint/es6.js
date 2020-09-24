694b36ad6bfd756f90e9019f337f64e5ff8001dc
Taha Chaoui <taha-chaoui@hotmail.com>
2020-09-24 19:58:47 +0100

    finished

---
+++ ES6 Basics Checkpoint/es6.js
@@ -0,0 +1,21 @@
+let pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+  
+  const getAge = (pet) => new Date().getFullYear() - pet.bornOn;
+  
+  pets.forEach( pet => {pet.age = getAge(pet)});
+  
+  console.log(pets)
+  
+  let dogs = pets.filter( pet => pet.type == "dog");
+  
+  console.log(dogs)
+  
+  const jasper = pets.find( pet => pet.name === 'Jasper')
+  
+  console.log(jasper);
+  
+  console.log(`Jasper is ${jasper.age} years old`);
